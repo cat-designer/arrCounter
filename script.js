4d7633c8ca577a4e251b440bff0f241cfa02fc4c
cat-designer <cat-designer@mail.ru>
2024-08-06 22:19:28 +0300

    1

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let arr = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ],  1, 8 ];
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-sum+= arr [i];
+let a = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ], 1, 8 ];
+
+function treeSum (arr) {
+	let s = 0;
+	for (let i = 0; i < arr.length; i++) {
+		let item = arr [i];
+		if (typeof item === 'Number') 
+			s+=item;
+		 else 
+			s+=treeSum (item);
+		}
+	return s;
 }
+
+let sum = treeSum (a);
 console.log (sum);
