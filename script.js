645ad3aaecdeded6e38df5f45f6ad61f8c8e8f56
cat-designer <you@example.com>
2024-08-06 20:48:58 +0300

    q

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let arr = [ 5, 7, [ 4, [2], 8, [1,3], 2 ], [ 9, [] ],  1, 8 ];
 let sum = 0;
-for (let i = 0; i < arr.length; i++){
-sum+= arr [i];}
+for (let i = 0; i < arr.length; i++) {
+sum+= arr [i];
+}
 console.log (sum);
